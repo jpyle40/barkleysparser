f0cfa28e5544227e6076f7a3b24c8fc8e16e9fa6
jpyle40 <jamesmpyle@googlemail.com>
2026-09-14 18:52:06 -0400

    Update to main_parser_work

---
+++ rust-json-parser/src/tokenizer.rs
@@ -0,0 +1,15 @@
+//Example stub:
+pub fn tokenize(input: &str) -> Vec<Token> {
+    Vec::new() //Empty vector - flesh out step by step
+}
+
+
+
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    // Tests will be added here, one step at a time.
+}
