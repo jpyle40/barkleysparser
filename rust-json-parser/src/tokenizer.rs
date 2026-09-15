df530cd6d9142690fa0a8eae6acd2fa987826b7f
jpyle40 <jamesmpyle@googlemail.com>
2026-09-15 09:49:35 -0400

    change branch name to week1

--- rust-json-parser/src/tokenizer.rs
+++ rust-json-parser/src/tokenizer.rs
@@ -1,3 +1,17 @@
+#[derive(Debug, Clone, PartialEq)]
+enum Token {
+    LeftBrace,
+    RightBrace,
+    LeftBracket,
+    RightBracket,
+    Comma,
+    String,
+    Number,
+    Boolean,
+    Null,
+}
+
+
 //Example stub:
 pub fn tokenize(input: &str) -> Vec<Token> {
     Vec::new() //Empty vector - flesh out step by step
@@ -12,4 +26,11 @@ mod tests {
     use super::*;
 
     // Tests will be added here, one step at a time.
+    #[test]
+    fn test_empty_braces() {
+        let tokens = tokenize("{}");
+        assert_eq!(tokens.len(), 2);
+        assert_eq!(tokens[0], Token::LeftBrace);
+        assert_eq!(tokens[1], Token::RightBrace);
+    }
 }
